705a6bcd5510a64419119d967678ac776acc7386
adrxnl <162568872+adrxnl@users.noreply.github.com>
2024-04-22 17:09:34 -0500

    Implemented switch statement and display menu

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include "btree.hpp"
 
-
 void displayMenu();
 
 int main(){
     AVLTree userTree;
 
+    int userChoice = -1;
+
+    do{
+        displayMenu();
+        cin >> userChoice;
+        switch(userChoice){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3: 
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default: 
+        }
+    }while(userChoice != 6);
 
     return 0;
 }
@@ -13,5 +33,12 @@ int main(){
 
 
 void displayMenu(){
-    cout << "1. asldjfk";
+    cout << "Select an action to execute: \n\n";
+    cout << "1. Add student.\n";
+    cout << "2. Remove student.\n";
+    cout << "3. Search student by ID.\n";
+    cout << "4. Search student by name.\n";
+    cout << "5. Update student info.\n";
+    cout << "6. Exit.\n";
+    cout << "\nEnter selection: ";
 }
